ce51522ed005f861c5f467e7ae80def67cb8a28b
JOEL_SMITH <joelsmith5000@gmail.com>
2024-10-01 12:07:07 -0700

    add paginator to data table

--- src/app/shared/data-table/data-table.component.ts
+++ src/app/shared/data-table/data-table.component.ts
@@ -1,34 +1,55 @@
-import { Component, Injectable, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Injectable,
+  Input,
+  ViewChild,
+} from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource } from '@angular/material/table';
 import { NgModule } from '@angular/core';
 import { NgFor, CommonModule } from '@angular/common';
 import { CustomColumn } from '../../models/hockey-column-def';
-import { MatPaginatorModule } from '@angular/material/paginator'; // If you're using pagination
-import { MatSortModule } from '@angular/material/sort'; // If you're using sorting
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'app-data-table',
   standalone: true,
-  imports: [MatTableModule, NgFor, CommonModule],
+  imports: [MatTableModule, NgFor, CommonModule, MatPaginatorModule],
   templateUrl: './data-table.component.html',
   styleUrl: './data-table.component.scss',
 })
 @Injectable({ providedIn: 'root' })
 export class DataTableComponent {
-  @Input() dataSource: any[] = [];
+  @Input() entities: any[] = [];
   @Input() tableDetails: CustomColumn[] = [];
   public tableProperties: string[] = [];
   public tableHeaders: string[] = [];
+  public dataSource = new MatTableDataSource<any>();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    if (this.entities.length > 0) {
+      this.dataSource.paginator = this.paginator;
+    }
+  }
 
   ngOnInit(): void {
     this.tableProperties = this.tableDetails.map((td) => td.columnRef);
     this.tableHeaders = this.tableDetails.map((td) => td.columnHeader);
+  }
+
+  ngOnChanges() {
+    // Update MatTableDataSource whenever the input data changes
+    if (this.entities && this.entities.length > 0) {
+      this.dataSource.data = this.entities;
+    }
+
+    // Assign paginator if it has been initialized
+    if (this.paginator) {
+      this.dataSource.paginator = this.paginator;
+    }
   }
 }
